867e004b6a136d41ef127525b16809009004bdda
vinicius <vinicius.generoso2@etec.sp.gov.br>
2024-04-08 22:26:10 -0300

    aula0804

---
+++ DLL0804/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL0804
+{
+    class Cliente:Pessoa
+    {
+        protected int pontos;
+        
+        public void mostraPontos()
+        {
+            Console.WriteLine("\nPontos:{0}", this.pontos);
+        }
+
+        public void somaPontos(int pontos)
+        {
+            if( pontos < 1) {
+                Console.WriteLine("Não foi possível somar pontos");
+            } else 
+            { 
+            this.pontos = pontos;
+            this.mostraPontos();
+            }
+        }
+    }
+}
---
+++ DLL0804/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL0804
+{
+    public class Pessoa
+    {
+        public string nome;
+        public string email;
+        public string telefone;
+        
+        public void cadastrar()
+        {
+            Console.WriteLine("Digite seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite seu e-mail");
+            email = Console.ReadLine();
+            Console.WriteLine("Digite seu telefone:");
+            telefone = Console.ReadLine();
+        }
+        public void exibir()
+        {
+            Console.WriteLine("Nome:{0} \nEmail : {1} \nTelefone:{2}" ,this.nome, this.email,this.telefone);
+        }
+
+    }
+}
